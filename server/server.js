bc128069f6bbe6ff6df37fd85d9edcdf176bee2d
Anzar Ali <anzarali150799@gmail.com>
2022-10-14 20:19:55 +0530

    setup file and folder structure for the project

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const { chats } = require("./data/data");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const { id } = req.params;
+  const chat = chats.find((chat) => chat._id === id);
+  res.send(chat);
+});
+
+app.listen(PORT, () => console.log(`listening on the port ${PORT}`));
