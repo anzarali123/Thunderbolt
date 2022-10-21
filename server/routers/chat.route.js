84987fd93706b2516b74ae2336f50e94e7bd314f
Anzar Ali <anzarali150799@gmail.com>
2022-10-21 20:23:26 +0530

    built the controllers to creategroup removefromgroup addtogroup

--- server/routers/chat.route.js
+++ server/routers/chat.route.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
-const { accessChat } = require("../controllers/chatController");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  removeFromGroup,
+  addToGroup,
+} = require("../controllers/chatController");
 
 const chatRouter = express.Router();
 
 chatRouter.post("/", protect, accessChat);
-// chatRouter.get("/",protect,fetchChats);
-// chatRouter.post("/group",protect,createGroupChat);
-// chatRouter.put("/rename",protect,renameGroup);
-// chatRouter.put("/groupremove",protect,removeFromGroup);
-// chatRouter.put("/groupadd",protect,addToGroup);
+chatRouter.get("/", protect, fetchChats);
+chatRouter.post("/group", protect, createGroupChat);
+chatRouter.put("/rename", protect, renameGroup);
+chatRouter.put("/groupremove", protect, removeFromGroup);
+chatRouter.put("/groupadd", protect, addToGroup);
 
 module.exports = { chatRouter };
